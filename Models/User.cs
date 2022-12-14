0e31a32c249a335874832201c996955273ebd541
eduardo <eduardo.paes@lektek.com.br>
2022-12-14 08:35:11 -0300

    feat: add simple user model

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DotnetGraphql.Models
+{
+  public class User
+  {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }
+  }
+}
