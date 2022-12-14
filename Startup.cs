40827c883bba2aa733501ee222a404af6584de9d
eduardo <eduardo.paes@lektek.com.br>
2022-12-14 08:34:18 -0300

    feat: add setup to startup and program

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,40 @@
-namespace DotnetGraphql {
+namespace DotnetGraphql
+{
   public class Startup
   {
-    public void ConfigureService(IServiceCollection services) {
+    private readonly IConfiguration Configuration;
 
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public void ConfigureService(IServiceCollection services)
+    {
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseWebSockets();
+
+      app.UseRouting();
+
+      // app.UseEndpoints(endpoints =>
+      // {
+      //     endpoints.MapGraphQL();
+      // });
+
+      // app.UseGraphQLVoyager(new GraphQLVoyagerOptions()
+      // {
+      //     GraphQLEndPoint = "/graphql",
+      //     Path = "/graphql-voyager"
+      // });
     }
   }
 }
