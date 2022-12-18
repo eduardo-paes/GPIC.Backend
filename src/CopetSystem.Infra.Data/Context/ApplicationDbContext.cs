9a4229c6af2486c5b98a1b43ebc35db2d9a92532
eduardo <eduardo.paes@lektek.com.br>
2022-12-18 17:34:03 -0300

    feat(infra-data): add database logic to infra-data project

---
+++ src/CopetSystem.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CopetSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CopetSystem.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<MainArea> MainAreas { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<SubArea> SubAreas { get; set; }
+
+        public DbSet<ProgramType> ProgramTypes { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<StudentAssistanceScholarship> AssistanceScholarships { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
+
---
+++ src/CopetSystem.Infra.Data/EntitiesConfiguration/AreaConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CopetSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CopetSystem.Infra.Data.EntitiesConfiguration
+{
+	public class AreaConfiguration : IEntityTypeConfiguration<Area>
+    {
+        public void Configure(EntityTypeBuilder<Area> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Code).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.MainAreaId).IsRequired();
+
+            builder.HasData(
+                new Area(1, "Area 1", "ABC-123", 1),
+                new Area(2, "Area 2", "DEF-456", 2),
+                new Area(3, "Area 3", "GHI-789", 3)
+            );
+        }
+    }
+
