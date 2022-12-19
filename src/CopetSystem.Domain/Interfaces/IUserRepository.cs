c8182689f39d8a817e27e2497d8a1a5af7ff3748
eduardo <eduardo.paes@lektek.com.br>
2022-12-19 11:45:19 -0300

    feat(domain): add user repository

---
+++ src/CopetSystem.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CopetSystem.Domain.Entities;
+
+namespace CopetSystem.Domain.Interfaces
+{
+	public interface IUserRepository
+	{
+        Task<IEnumerable<User>> GetActiveUsers();
+        Task<IEnumerable<User>> GetInactiveUsers();
+
+        Task<User> GetById(int? id);
+        Task<User> GetByEmail(string? email);
+
+        Task<User> Create(User user);
+        Task<User> Update(User user);
+        Task<User> Remove(User user);
+    }
+}
+
