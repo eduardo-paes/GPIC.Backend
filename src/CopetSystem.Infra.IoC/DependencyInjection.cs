2ec556e05f8817f5f554b46266bd4f1fd6858b41
eduardo <eduardo.paes@lektek.com.br>
2022-12-19 11:45:57 -0300

    feat(infra-ioc): add basic api dependency injection logics

---
+++ src/CopetSystem.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using CopetSystem.Application.Interfaces;
+using CopetSystem.Application.Mappings;
+using CopetSystem.Application.Services;
+using CopetSystem.Domain.Interfaces;
+using CopetSystem.Infra.Data.Context;
+using CopetSystem.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CopetSystem.Infra.IoC;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+         options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"
+        ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        // Repositórios
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        // Serviços de Negócios
+        services.AddScoped<IUserService, UserService>();
+
+        // DTOs
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        //var myhandlers = AppDomain.CurrentDomain.Load("CopetSystem.Application");
+        //services.AddMediatR(myhandlers);
+
+        return services;
+    }
+}
+
