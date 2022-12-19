af74391f22375ee099ed457a9054e0680f36aad0
eduardo <eduardo.paes@lektek.com.br>
2022-12-19 09:19:19 -0300

    feat(tests): add first domain test

---
+++ src/CopetSystem.Domain.Tests/UserUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using CopetSystem.Domain.Entities;
+using FluentAssertions;
+
+namespace CopetSystem.Domain.Tests;
+public class UserUnitTests
+{
+    [Fact(DisplayName = "Create User With Valid State")]
+    public void CreateUser_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = () => new User("User Name", "username@gmail.com", "123456", "15162901784", "TEST");
+        action.Should()
+             .NotThrow<CopetSystem.Domain.Validation.DomainExceptionValidation>();
+    }
+}
+
