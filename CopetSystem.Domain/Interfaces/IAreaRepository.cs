c02d9ce7f68ec1b027185750f3757cceac5da9f8
eduardo <eduardo.paes@lektek.com.br>
2023-02-09 09:23:23 -0300

    feat: add areaRepository

---
+++ CopetSystem.Domain/Interfaces/IAreaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CopetSystem.Domain.Entities;
+
+namespace CopetSystem.Domain.Interfaces
+{
+	public interface IAreaRepository
+	{
+        Task<Area> Get();
+        Task<IEnumerable<Area>> GetAll();
+
+        Task<Area> Create(Area area);
+		Task<Area> Remove(Area area);
+		Task<Area> Update(Area area);
+    }
+}
+
