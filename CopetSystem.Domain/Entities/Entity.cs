6814b86f4f9809c735bed14d0fa9f88320d6e53c
eduardo <eduardo.paes@lektek.com.br>
2023-02-09 09:21:35 -0300

    feat: add guid to entity

--- CopetSystem.Domain/Entities/Entity.cs
+++ CopetSystem.Domain/Entities/Entity.cs
@@ -3,7 +3,7 @@ namespace CopetSystem.Domain.Entities
 {
 	public abstract class Entity
 	{
-		public long? Id { get; protected set; }
+		public Guid? Id { get; protected set; }
         public DateTime? DeletedAt { get; protected set; }
 
         public void DeactivateEntity() => DeletedAt = DateTime.Now;
