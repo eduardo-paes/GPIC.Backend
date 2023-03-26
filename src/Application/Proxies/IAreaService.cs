2ab86e195c2e9ccdbd49cc35a21f8f28c8f4b36a
Eduardo Paes <eduardo-paes@outlook.com>
2023-03-26 12:32:38 -0300

    feat: add generic crud service

--- src/Application/Proxies/IAreaService.cs
+++ src/Application/Proxies/IAreaService.cs
@@ -1,40 +1,6 @@
 namespace Application.Proxies.Area
 {
-    public public interface IAreaService
+    public public interface IAreaService : IGenericCRUDService
     {
-        /// <summary>
-        /// Busca entidade pelo Id informado.
-        /// Lança uma exceção quando a entidade não é encontrada.
-        /// </summary>
-        /// <param name="id">Id da entidade.</param>
-        /// <returns>Entidade encontrada.</returns>
-        Task<DetailedReadAreaDTO> GetById(Guid? id);
-
-        /// <summary>
-        /// Busca todas as entidades ativas.
-        /// </summary>
-        /// <returns>Lista de entidades ativas.</returns>
-        Task<IEnumerable<ResumedReadAreaDTO>> GetAll(int skip, int take);
-
-        /// <summary>
-        /// Cria entidade conforme parâmetros fornecidos.
-        /// </summary>
-        /// <param name="model">Parâmetros de criação.</param>
-        /// <returns>Entidade criada.</returns>
-        Task<ResumedReadAreaDTO> Create(CreateAreaDTO model);
-
-        /// <summary>
-        /// Remove entidade através do Id informado.
-        /// </summary>
-        /// <param name="id">Id da entidade a ser removida.</param>
-        /// <returns>Entidade removida.</returns>
-        Task<ResumedReadAreaDTO> Delete(Guid? id);
-
-        /// <summary>
-        /// Atualiza entidade conforme parâmetros fornecidos.
-        /// </summary>
-        /// <param name="model">Parâmetros de atualização.</param>
-        /// <returns>Entidade atualizada.</returns>
-        Task<ResumedReadAreaDTO> Update(UpdateAreaDTO model);
     }
 }
--- src/Application/Proxies/IMainAreaService.cs
+++ src/Application/Proxies/IMainAreaService.cs
@@ -1,40 +1,6 @@
 namespace Application.Proxies.MainArea
 {
-    public public interface IMainAreaService
+    public public interface IMainAreaService : IGenericCRUDService
     {
-        /// <summary>
-        /// Busca entidade pelo Id informado.
-        /// Lança uma exceção quando a entidade não é encontrada.
-        /// </summary>
-        /// <param name="id">Id da entidade.</param>
-        /// <returns>Entidade encontrada.</returns>
-        Task<DetailedReadMainAreaDTO> GetById(Guid? id);
-
-        /// <summary>
-        /// Busca todas as entidades ativas.
-        /// </summary>
-        /// <returns>Lista de entidades ativas.</returns>
-        Task<IEnumerable<ResumedReadMainAreaDTO>> GetAll(int skip, int take);
-
-        /// <summary>
-        /// Cria entidade conforme parâmetros fornecidos.
-        /// </summary>
-        /// <param name="model">Parâmetros de criação.</param>
-        /// <returns>Entidade criada.</returns>
-        Task<ResumedReadMainAreaDTO> Create(CreateMainAreaDTO model);
-
-        /// <summary>
-        /// Remove entidade através do Id informado.
-        /// </summary>
-        /// <param name="id">Id da entidade a ser removida.</param>
-        /// <returns>Entidade removida.</returns>
-        Task<ResumedReadMainAreaDTO> Delete(Guid? id);
-
-        /// <summary>
-        /// Atualiza entidade conforme parâmetros fornecidos.
-        /// </summary>
-        /// <param name="model">Parâmetros de atualização.</param>
-        /// <returns>Entidade atualizada.</returns>
-        Task<ResumedReadMainAreaDTO> Update(UpdateMainAreaDTO model);
     }
 }
