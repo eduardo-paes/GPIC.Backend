989f483905e82fc79d9fbd147b898436b96b49a4
Eduardo Paes <eduardo-paes@outlook.com>
2023-03-26 12:29:39 -0300

    refactor: update create area dto

--- src/Application/DTOs/Area/CreateAreaDTO.cs
+++ src/Application/DTOs/Area/CreateAreaDTO.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Application.DTOs.Base;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs.Area
 {
-    public class CreateAreaDTO : BaseAreaDTO
+    public class CreateAreaDTO : RequestDTO
     {
         [Required]
         public Guid? MainAreaId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Code { get; set; }
     }
 }
 
