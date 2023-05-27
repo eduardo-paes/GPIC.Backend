f996365f0083413d8ba29c8c86b66da4ac23a3f8
eduardo <eduardo.paes@lektek.com.br>
2023-05-27 11:56:35 -0300

    refactor: modify exception messages to english

--- src/Domain/Validation/ExceptionMessageFactory.cs
+++ src/Domain/Validation/ExceptionMessageFactory.cs
@@ -1,14 +1,12 @@
 ﻿namespace Domain.Validation;
 public static class ExceptionMessageFactory
 {
-    public static string MinLength(string prop, int length) => $"Valor do campo ({prop}) muito curto, o mínimo de caracteres é {length}.";
-    public static string MaxLength(string prop, int length) => $"Valor do campo ({prop}) muito grande, o máximo de caracteres é {length}.";
-    public static string WithLength(string prop, int length) => $"Valor do campo ({prop}) inválido, o número de caracteres deve ser {length}.";
-    public static string Required(string prop) => $"Valor do campo ({prop}) inválido. {FirstCharToUpper(prop)} precisa ser preenchido.";
-    public static string Invalid(string prop) => $"Valor do campo ({FirstCharToUpper(prop)}) inválido.";
-    public static string InvalidEmail(string prop) => $"Valor do campo ({FirstCharToUpper(prop)}) inválido. O e-mail informado não é válido.";
-    public static string InvalidCpf() => $"Valor de CPF inválido. Sequência de números não é um CPF válido pela lógica do governo.";
-    public static string LessThan(string prop, string value) => $"O valor de ({prop}) não pode ser menor do que {value}.";
-
-    private static string FirstCharToUpper(string input) => input.FirstOrDefault().ToString().ToUpper() + input[1..];
+    public static string MinLength(string prop, int length) => $"The value of ({prop}) is too short. The minimum length is {length} characters.";
+    public static string MaxLength(string prop, int length) => $"The value of ({prop}) is too long. The maximum length is {length} characters.";
+    public static string WithLength(string prop, int length) => $"Invalid value for ({prop}). The number of characters should be {length}.";
+    public static string Required(string prop) => $"Invalid value for ({prop}). {prop} must be provided.";
+    public static string Invalid(string prop) => $"Invalid value for ({prop}).";
+    public static string InvalidEmail(string prop) => $"Invalid value for ({prop}). The provided email is not valid.";
+    public static string InvalidCpf() => "Invalid CPF value. The number sequence is not a valid CPF according to government logic.";
+    public static string LessThan(string prop, string value) => $"The value of ({prop}) cannot be less than {value}.";
 }
