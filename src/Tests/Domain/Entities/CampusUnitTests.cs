28d4bedd686c318329dcae5525c00dcba96a79a3
eduardo <eduardo.paes@lektek.com.br>
2023-05-30 12:36:45 -0300

    feat(tests): add campus unit tests

---
+++ src/Tests/Domain/Entities/CampusUnitTests.cs
@@ -0,0 +1,57 @@
+using Domain.Entities;
+using Domain.Validation;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Domain.Entities
+{
+    public class CampusTests
+    {
+        private Campus MockValidCampus() => new Campus("Campus Name");
+
+        [Fact]
+        public void SetName_ValidName_SetsName()
+        {
+            // Arrange
+            var campus = MockValidCampus();
+            var name = "Campus Name";
+
+            // Act
+            campus.Name = name;
+
+            // Assert
+            campus.Name.Should().Be(name);
+        }
+
+        [Fact]
+        public void SetName_NullOrEmptyName_ThrowsException()
+        {
+            // Arrange
+            var campus = MockValidCampus();
+
+            // Act & Assert
+            Assert.Throws<EntityExceptionValidation>(() => campus.Name = null);
+            Assert.Throws<EntityExceptionValidation>(() => campus.Name = string.Empty);
+        }
+
+        [Fact]
+        public void SetName_TooShortName_ThrowsException()
+        {
+            // Arrange
+            var campus = MockValidCampus();
+
+            // Act & Assert
+            Assert.Throws<EntityExceptionValidation>(() => campus.Name = "AB");
+        }
+
+        [Fact]
+        public void SetName_TooLongName_ThrowsException()
+        {
+            // Arrange
+            var campus = MockValidCampus();
+
+            // Act & Assert
+            Assert.Throws<EntityExceptionValidation>(() => campus.Name = "lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris ullamcorper. lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris ullamcorper. lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris ullamcorper. lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris ullamcorper. lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris ullamcorper.");
+        }
+    }
+}
