6aa00eabb1ea9e6cd16295445466d95c167c864e
eduardo <eduardo.paes@lektek.com.br>
2023-07-03 20:33:45 -0300

    feat(presenter-controllers): add student-documents

---
+++ src/Adapters/Gateways/StudentDocuments/CreateStudentDocumentsRequest.cs
@@ -0,0 +1,5 @@
+﻿using Adapters.Gateways.Base;
+using Domain.Contracts.StudentDocuments;
+
+namespace Adapters.Gateways.StudentDocuments;
+public class CreateStudentDocumentsRequest : CreateStudentDocumentsInput, IRequest { }
