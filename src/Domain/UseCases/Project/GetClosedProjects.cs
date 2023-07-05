510ba68f1b59de7f07517a623a1809a8d07de9d4
eduardo <eduardo.paes@lektek.com.br>
2023-07-04 21:16:50 -0300

    fix(project): adjust user-role validation

--- src/Domain/UseCases/Project/GetClosedProjects.cs
+++ src/Domain/UseCases/Project/GetClosedProjects.cs
@@ -29,28 +29,35 @@ namespace Domain.UseCases.Project
             // Obtém as claims do usuário autenticado.
             var userClaims = _tokenAuthenticationService.GetUserAuthenticatedClaims();
 
+            // Se o usuário não estiver autenticado, lança uma exceção.
+            UseCaseException.BusinessRuleViolation(userClaims == null || userClaims.Role == null,
+                "Not authorized user.");
+
+            // Obtém o tipo de usuário.
+            var userRole = Enum.Parse<ERole>(userClaims?.Role!);
+
             // Obtém a lista de projetos de acordo com o tipo de usuário.
             IEnumerable<Entities.Project> projects;
 
             // Se o usuário for um professor, retorna apenas os seus projetos.
-            if (userClaims.Role == ERole.PROFESSOR.GetDescription())
-                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims.Id, true);
+            if (userRole == ERole.PROFESSOR)
+                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims?.Id, true);
 
             // Se o usuário for um aluno, retorna apenas os seus projetos.
-            else if (userClaims.Role == ERole.STUDENT.GetDescription())
-                projects = await _projectRepository.GetStudentProjects(skip, take, userClaims.Id, true);
+            else if (userRole == ERole.STUDENT)
+                projects = await _projectRepository.GetStudentProjects(skip, take, userClaims?.Id, true);
 
             // Se o usuário for um administrador, permite a busca apenas pelo seu ID.
-            else if (userClaims.Role == ERole.ADMIN.GetDescription() && onlyMyProjects)
-                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims.Id, true);
+            else if (userRole == ERole.ADMIN && onlyMyProjects)
+                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims?.Id, true);
 
             // Se o usuário for um administrador, permite a busca por todos os projetos.
-            else if (userClaims.Role == ERole.ADMIN.GetDescription() && !onlyMyProjects)
+            else if (userRole == ERole.ADMIN && !onlyMyProjects)
                 projects = await _projectRepository.GetProjects(skip, take, true);
 
             // Se o usuário não for nenhum dos tipos acima, lança uma exceção.
             else
-                throw UseCaseException.BusinessRuleViolation("Unauthorized user.");
+                throw UseCaseException.BusinessRuleViolation("Not authorized user.");
 
             // Mapeia a lista de projetos para uma lista de projetos resumidos e retorna.
             return _mapper.Map<IList<ResumedReadProjectOutput>>(projects);
--- src/Domain/UseCases/Project/GetOpenProjects.cs
+++ src/Domain/UseCases/Project/GetOpenProjects.cs
@@ -4,6 +4,7 @@ using Domain.Entities.Enums;
 using Domain.Interfaces.Repositories;
 using Domain.Interfaces.Services;
 using Domain.Interfaces.UseCases.Project;
+using Domain.Validation;
 
 namespace Domain.UseCases.Project
 {
@@ -28,28 +29,35 @@ namespace Domain.UseCases.Project
             // Obtém as claims do usuário autenticado.
             var userClaims = _tokenAuthenticationService.GetUserAuthenticatedClaims();
 
+            // Se o usuário não estiver autenticado, lança uma exceção.
+            UseCaseException.BusinessRuleViolation(userClaims == null || userClaims.Role == null,
+                "Not authorized user.");
+
+            // Obtém o tipo de usuário.
+            var userRole = Enum.Parse<ERole>(userClaims?.Role!);
+
             // Obtém a lista de projetos de acordo com o tipo de usuário.
             IEnumerable<Entities.Project> projects;
 
             // Se o usuário for um professor, retorna apenas os seus projetos.
-            if (userClaims.Role == ERole.PROFESSOR.GetDescription())
-                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims.Id);
+            if (userRole == ERole.PROFESSOR)
+                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims?.Id);
 
             // Se o usuário for um aluno, retorna apenas os seus projetos.
-            else if (userClaims.Role == ERole.STUDENT.GetDescription())
-                projects = await _projectRepository.GetStudentProjects(skip, take, userClaims.Id);
+            else if (userRole == ERole.STUDENT)
+                projects = await _projectRepository.GetStudentProjects(skip, take, userClaims?.Id);
 
             // Se o usuário for um administrador, permite a busca apenas pelo seu ID.
-            else if (userClaims.Role == ERole.ADMIN.GetDescription() && onlyMyProjects)
-                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims.Id);
+            else if (userRole == ERole.ADMIN && onlyMyProjects)
+                projects = await _projectRepository.GetProfessorProjects(skip, take, userClaims?.Id);
 
             // Se o usuário for um administrador, permite a busca por todos os projetos.
-            else if (userClaims.Role == ERole.ADMIN.GetDescription() && !onlyMyProjects)
+            else if (userRole == ERole.ADMIN && !onlyMyProjects)
                 projects = await _projectRepository.GetProjects(skip, take);
 
             // Se o usuário não for nenhum dos tipos acima, lança uma exceção.
             else
-                throw new Exception("Usuário não autorizado.");
+                throw UseCaseException.BusinessRuleViolation("Not authorized user.");
 
             // Mapeia a lista de projetos para uma lista de projetos resumidos e retorna.
             return _mapper.Map<IList<ResumedReadProjectOutput>>(projects);
