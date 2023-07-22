80837c5e33d97824917b8ad55c6de5a4e177984b
eduardo <eduardo.paes@lektek.com.br>
2023-07-22 16:01:48 -0300

    feat: add acitivity type entity

---
+++ src/Domain/Entities/ActivityType.cs
@@ -0,0 +1,72 @@
+using Domain.Entities.Primitives;
+using Domain.Validation;
+
+namespace Domain.Entities
+{
+    public class ActivityType : Entity
+    {
+        #region Properties
+        private string? _name;
+        public string? Name
+        {
+            get { return _name; }
+            set
+            {
+                EntityExceptionValidation.When(string.IsNullOrEmpty(value),
+                    ExceptionMessageFactory.Required(nameof(Name)));
+                EntityExceptionValidation.When(value?.Length < 3,
+                    ExceptionMessageFactory.MinLength(nameof(Name), 3));
+                EntityExceptionValidation.When(value?.Length > 300,
+                    ExceptionMessageFactory.MaxLength(nameof(Name), 300));
+                _name = value;
+            }
+        }
+
+        private string? _unity;
+        public string? Unity
+        {
+            get { return _unity; }
+            set
+            {
+                EntityExceptionValidation.When(string.IsNullOrEmpty(value),
+                    ExceptionMessageFactory.Required(nameof(Unity)));
+                EntityExceptionValidation.When(value?.Length < 3,
+                    ExceptionMessageFactory.MinLength(nameof(Unity), 3));
+                EntityExceptionValidation.When(value?.Length > 300,
+                    ExceptionMessageFactory.MaxLength(nameof(Unity), 300));
+                _unity = value;
+            }
+        }
+
+        private Guid? _noticeId;
+        public Guid? NoticeId
+        {
+            get => _noticeId;
+            private set
+            {
+                {
+                    EntityExceptionValidation.When(value == null,
+                        ExceptionMessageFactory.Required("Id do Edital"));
+                    _noticeId = value;
+                }
+            }
+        }
+
+        public virtual Notice? Notice { get; }
+        #endregion
+
+        #region Constructors
+        public ActivityType(string? name, string? unity, Guid? noticeId)
+        {
+            Name = name;
+            Unity = unity;
+            NoticeId = noticeId;
+        }
+
+        /// <summary>
+        /// Constructor to dbcontext EF instancing.
+        /// </summary>
+        protected ActivityType() { }
+        #endregion
+    }
+}
---
+++ src/Domain/Interfaces/Repositories/IActivityTypeRepository.cs
@@ -0,0 +1,10 @@
+using Domain.Entities;
+using Domain.Interfaces.Repositories.Bases;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IActivityTypeRepository : IGenericCRUDRepository<ActivityType>
+    {
+        Task<IEnumerable<ActivityType>> GetByNoticeId(Guid? noticeId);
+    }
+}
