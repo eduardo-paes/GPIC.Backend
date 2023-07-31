4375b354316d8f53cdefd61f435eb4fc55e2ec13
eduardo <eduardo.paes@lektek.com.br>
2023-07-30 21:44:48 -0300

    feat: add cors configuration

--- src/Infrastructure/WebAPI/Program.cs
+++ src/Infrastructure/WebAPI/Program.cs
@@ -1,15 +1,20 @@
-namespace Infrastructure.WebAPI;
-/// <summary>
-/// Classe de iniciação da WebAPI.
-/// </summary>
-public static class Program
+namespace WebAPI
 {
     /// <summary>
-    /// Método principal da WebAPI.
+    /// Classe de iniciação da WebAPI.
     /// </summary>
-    public static void Main(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-            .Build()
-            .Run();
+    public static class Program
+    {
+        /// <summary>
+        /// Método principal da WebAPI.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
 }
--- src/Infrastructure/WebAPI/Startup.cs
+++ src/Infrastructure/WebAPI/Startup.cs
@@ -2,77 +2,93 @@ using AspNetCoreRateLimit;
 using Infrastructure.IoC;
 using Infrastructure.WebAPI.Middleware;
 
-namespace Infrastructure.WebAPI;
-
-/// <summary>
-/// Classe de iniciação da WebAPI.
-/// </summary>
-public class Startup
+namespace WebAPI
 {
     /// <summary>
-    /// Realiza a configuração dos serviços de injeção de dependência.
+    /// Classe de iniciação da WebAPI.
     /// </summary>
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-        // Adição dos Controllers
-        services.AddControllers();
-
-        // Realiza comunicação com os demais Projetos.
-        services.AddInfrastructure();
-        services.AddAdapters();
-        services.AddDomain();
-
-        // Configuração do Swagger
-        services.AddInfrastructureSwagger();
-
-        // Configuração do JWT
-        services.AddInfrastructureJWT();
-
-        // Permite que rotas sejam acessíveis em lowercase
-        services.AddRouting(options => options.LowercaseUrls = true);
-    }
-
-    /// <summary>
-    /// Adiciona as configurações de segurança, documentação e roteamento.
-    /// </summary>
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
+        /// <summary>
+        /// Realiza a configuração dos serviços de injeção de dependência.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
         {
-            // Show detailed error page in development mode
-            app.UseDeveloperExceptionPage();
+            // Adição dos Controllers
+            _ = services.AddControllers();
 
-            // Enable Swagger middleware for API documentation in development mode
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            // Realiza comunicação com os demais Projetos.
+            _ = services.AddInfrastructure();
+            _ = services.AddAdapters();
+            _ = services.AddDomain();
 
-            // Show development mode message
-            Console.WriteLine("Development mode");
+            // Configuração do Swagger
+            _ = services.AddInfrastructureSwagger();
+
+            // Configuração do JWT
+            _ = services.AddInfrastructureJWT();
+
+            // Permite que rotas sejam acessíveis em lowercase
+            _ = services.AddRouting(options => options.LowercaseUrls = true);
+
+            // Configuração do CORS
+            _ = services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        _ = policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
         }
 
-        // UseExceptionHandler for non-development environments
-        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        /// <summary>
+        /// Adiciona as configurações de segurança, documentação e roteamento.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                // Show detailed error page in development mode
+                _ = app.UseDeveloperExceptionPage();
 
-        // Enable HTTP Strict Transport Security (HSTS) headers for secure communication
-        app.UseHsts();
+                // Enable Swagger middleware for API documentation in development mode
+                _ = app.UseSwagger();
+                _ = app.UseSwaggerUI();
 
-        // Redirect HTTP requests to HTTPS for secure communication
-        app.UseHttpsRedirection();
+                // Show development mode message
+                Console.WriteLine("Development mode");
+            }
 
-        // Enable routing for incoming requests
-        app.UseRouting();
+            // UseExceptionHandler for non-development environments
+            _ = app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-        // Enable authentication for the API
-        app.UseAuthentication();
+            // Enable HTTP Strict Transport Security (HSTS) headers for secure communication
+            _ = app.UseHsts();
 
-        // Enable authorization for the API
-        app.UseAuthorization();
+            // Redirect HTTP requests to HTTPS for secure communication
+            _ = app.UseHttpsRedirection();
 
-        // Apply rate limiting middleware to control the number of requests allowed  
-        app.UseClientRateLimiting();
-        app.UseIpRateLimiting();
+            // Enable routing for incoming requests
+            _ = app.UseRouting();
 
-        // Configure API endpoints
-        app.UseEndpoints(endpoints => endpoints.MapControllers());
+            // Enable authentication for the API
+            _ = app.UseAuthentication();
+
+            // Enable authorization for the API
+            _ = app.UseAuthorization();
+
+            // Apply rate limiting middleware to control the number of requests allowed  
+            _ = app.UseClientRateLimiting();
+            _ = app.UseIpRateLimiting();
+
+            // Configure API endpoints
+            _ = app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
     }
 }
