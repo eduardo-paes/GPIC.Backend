e4be59028a8a4e6e595e061e7330ed62edb4d2dd
eduardo <eduardo.paes@lektek.com.br>
2023-08-16 21:17:45 -0300

    feat: add more info to ReportService

--- src/Infrastructure/Services/ReportService.cs
+++ src/Infrastructure/Services/ReportService.cs
@@ -17,19 +17,29 @@ namespace Services
             // Caminho temporário onde será salvo o arquivo
             string outputPath = "";
 
+            // Obtém o semestre do edital
+            string noticeDate;
+            if (project!.Notice!.RegistrationStartDate!.Value.Month > 6)
+                noticeDate = $"{project.Notice.RegistrationStartDate.Value.Year} / 2";
+            else
+                noticeDate = $"{project.Notice.RegistrationStartDate.Value.Year} / 1";
+
+            // Obtém a situação do aluno (Bolsista ou Voluntário)
+            var studentSituation = project.IsScholarshipCandidate ? "Término do Período de Bolsa" : "Término do Período de Voluntariado";
+
             // Obtém o conteúdo do arquivo HTML
             string template = await File.ReadAllTextAsync(Path.Combine(_currentDirectory!, "Reports/certificate.html"));
 
             // Substitui as variáveis do HTML pelos valores do projeto
             template = template
                 .Replace("#NOME_ORIENTADOR#", project?.Professor?.User?.Name)
-                .Replace("#NOME_DEPARTAMENTO#", "") // TODO: Inserir departamento do professor no Projeto?
+                .Replace("#SUBAREA_PROJETO#", project?.SubArea?.Name)
                 .Replace("#NOME_ORIENTADO#", project?.Student?.User?.Name)
-                .Replace("#DATA_EDITAL#", "") // TODO: Verificar como obter a data do edital (Ex.: 2017 / 2)
+                .Replace("#DATA_EDITAL#", noticeDate)
                 .Replace("#PIBIC_TIPO#", $"{project?.ProgramType?.Name} / CEFET")
-                .Replace("#INIP_EDITAL#", project?.Notice?.SendingDocsEndDate?.ToString("dd/MM/yyyy")) // TODO: Validar essa informação
-                .Replace("#FIMP_EDITAL#", project?.Notice?.FinalReportDeadline?.ToString("dd/MM/yyyy")) // TODO: Validar essa informação
-                .Replace("#SITP_EDITAL#", "") // TODO: Pegar essa informação do Projeto?
+                .Replace("#INIP_EDITAL#", project?.Notice?.SendingDocsEndDate?.ToString("dd/MM/yyyy"))
+                .Replace("#FIMP_EDITAL#", project?.Notice?.FinalReportDeadline?.ToString("dd/MM/yyyy"))
+                .Replace("#SITP_EDITAL#", studentSituation) // TODO: Pegar essa informação do Projeto?
                 .Replace("#TITULO_PROJETO_ALUNO#", project?.Title)
                 .Replace("#TITULO_PROJETO_ORIENTADOR#", "") // TODO: Verificar se esse campo será inserido no Projeto
                 .Replace("#DIA_SEMANA#", DateTime.Now.DayOfWeek.ToString())
