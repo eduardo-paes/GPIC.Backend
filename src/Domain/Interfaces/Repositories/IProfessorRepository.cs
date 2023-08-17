8a71658b858dd0c8a0b9290f0a99435166324883
eduardo <eduardo.paes@lektek.com.br>
2023-08-17 15:09:00 -0300

    feat: add MakeAdmin usecase

--- src/Domain/Interfaces/Repositories/IProfessorRepository.cs
+++ src/Domain/Interfaces/Repositories/IProfessorRepository.cs
@@ -6,5 +6,6 @@ namespace Domain.Interfaces.Repositories
     public interface IProfessorRepository : IGenericCRUDRepository<Professor>
     {
         Task<IEnumerable<Professor>> GetAllActiveProfessorsAsync();
+        Task<Professor?> GetByUserIdAsync(Guid? userId);
     }
 }
