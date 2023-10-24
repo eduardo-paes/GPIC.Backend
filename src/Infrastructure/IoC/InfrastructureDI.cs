eb8cde421b565331008d8719e335ee5b4b00d391
eduardo <eduardo.paes@lektek.com.br>
2023-10-24 10:23:16 -0300

    feat: Add CORS configuration

--- src/Infrastructure/IoC/InfrastructureDI.cs
+++ src/Infrastructure/IoC/InfrastructureDI.cs
@@ -11,11 +11,11 @@ namespace Infrastructure.IoC
 {
     public static class InfrastructureDI
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, HostBuilderContext? hostContext = null)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ref IConfiguration? configuration, HostBuilderContext? hostContext = null)
         {
             #region AppSettings e DotEnv
             // Define valores das propriedades de configuração
-            IConfiguration configuration = SettingsConfiguration.GetConfiguration(hostContext);
+            configuration = SettingsConfiguration.GetConfiguration(hostContext);
             services.AddSingleton(configuration);
 
             // Carrega informações de ambiente (.env)
@@ -34,30 +34,6 @@ namespace Infrastructure.IoC
                 loggingBuilder.AddSerilog(Log.Logger, dispose: true);
             });
             #endregion Serviço de Log
-
-            #region CORS
-            // TODO: Definir política de CORS
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    policy =>
-                    {
-                        policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                    });
-            });
-            #endregion CORS
-
-            #region Rate Limit
-            services.AddMemoryCache();
-            services.AddInMemoryRateLimiting();
-            services.Configure<ClientRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
-            services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>();
-            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
-            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
-            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-            #endregion Rate Limit
 
             return services;
         }
